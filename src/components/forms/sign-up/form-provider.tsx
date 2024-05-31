a6d4ee628c73b7cf107440e9572c8fbe903eebf5
awais512 <raza.awais25@gmail.com>
2024-05-31 22:38:43 +0100

    User Registration with custom clerk auth done

---
+++ src/components/forms/sign-up/form-provider.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { Loader } from "@/components/loader";
+import { AuthContextProvider } from "@/context/use-auth-context";
+import { useSignUpForm } from "@/hooks/sign-up/use-sign-up";
+import { FormProvider } from "react-hook-form";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const SignUpFormProvider = ({ children }: Props) => {
+  const { methods, onHandleSubmit, loading } = useSignUpForm();
+  return (
+    <AuthContextProvider>
+      <FormProvider {...methods}>
+        <form onSubmit={onHandleSubmit} className="h-full">
+          <div className="flex flex-col justify-between gap-3 h-full">
+            <Loader loading={loading}>{children}</Loader>
+          </div>
+        </form>
+      </FormProvider>
+    </AuthContextProvider>
+  );
+};
