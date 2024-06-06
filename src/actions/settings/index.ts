e49d378ce618151686fccc71aebd4e75f524a97d
awais512 <raza.awais25@gmail.com>
2024-06-06 23:58:21 +0100

    Update password

--- src/actions/settings/index.ts
+++ src/actions/settings/index.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { client } from "@/lib/prisma";
-import { currentUser } from "@clerk/nextjs";
+import { clerkClient, currentUser } from "@clerk/nextjs";
 
 export const onGetSubscriptionPlan = async () => {
   try {
@@ -140,6 +140,19 @@ export const onIntegrateDomain = async (domain: string, icon: string) => {
       status: 400,
       message: "Domain already exists",
     };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const onUpdatePassword = async (password: string) => {
+  try {
+    const user = await currentUser();
+    if (!user) return null;
+    const update = await clerkClient.users.updateUser(user.id, { password });
+    if (update) {
+      return { status: 200, message: "Password updated" };
+    }
   } catch (error) {
     console.log(error);
   }
--- src/hooks/settings/use-settings.ts
+++ src/hooks/settings/use-settings.ts
@@ -1,5 +1,15 @@
+import { onUpdatePassword } from "@/actions/settings";
+import { useToast } from "@/components/ui/use-toast";
+import {
+  ChangePasswordProps,
+  ChangePasswordSchema,
+} from "@/schemas/auth.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { UploadClient } from "@uploadcare/upload-client";
 import { useTheme } from "next-themes";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 const upload = new UploadClient({
   publicKey: process.env.NEXT_PUBLIC_UPLOAD_CARE_PUBLIC_KEY as string,
@@ -11,5 +21,45 @@ export const useThemeMode = () => {
   return {
     setTheme,
     theme,
+  };
+};
+
+export const useChangePassword = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<ChangePasswordProps>({
+    resolver: zodResolver(ChangePasswordSchema),
+    mode: "onChange",
+  });
+
+  const { toast } = useToast();
+
+  const onChangePassword = handleSubmit(async (values) => {
+    try {
+      setLoading(true);
+      const updated = await onUpdatePassword(values.password);
+      if (updated) {
+        reset();
+        setLoading(false);
+        toast({
+          title: "Success",
+          description: updated.message,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  return {
+    onChangePassword,
+    loading,
+    register,
+    errors,
   };
 };
